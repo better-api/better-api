b8656edc6724e4a39ddfe4f3fdcc9628beb34708
Vid Drobnič <me@viddrobnic.com>
2025-10-07 21:55:35 +0200

    start defining ast nodes

--- syntax/node.rs
+++ syntax/node.rs
@@ -1,15 +1,24 @@
+use crate::Kind;
 use crate::Kind::*;
 use crate::Language;
 
 pub type SyntaxNode = rowan::SyntaxNode<Language>;
 
-// Helper macro to generator AST nodes.
+// Helper macros to generator AST nodes as structs.
 macro_rules! ast_node {
-    ($ast:ident, $kind:ident) => {
-        #[derive(PartialEq, Eq, Hash)]
+    (
+        #[from($kind:ident)]
+        $(#[$attr:meta])*
+        struct $name:ident;
+    ) => {
+        $(#[$attr])*
+        #[derive(Debug, Clone, PartialEq, Eq, Hash)]
         #[repr(transparent)]
-        pub struct $ast(SyntaxNode);
-        impl $ast {
+        pub struct $name(SyntaxNode);
+
+        impl $name {
+            pub const KIND: Kind = $kind;
+
             #[allow(unused)]
             pub(crate) fn cast(node: SyntaxNode) -> Option<Self> {
                 if node.kind() == $kind {
@@ -20,6 +29,116 @@ macro_rules! ast_node {
             }
         }
     };
+
+    (
+        #[from($($variant:ident),+ $(,)?)]
+        $(#[$attr:meta])*
+        enum $name:ident;
+    ) => {
+        $(#[$attr])*
+        #[derive(Debug, Clone, PartialEq, Eq, Hash)]
+        pub enum $name {
+            $($variant($variant)),+
+        }
+        impl $name {
+            #[allow(unused)]
+            pub(crate) fn cast(node: SyntaxNode) -> Option<Self> {
+                let node = match node.kind() {
+                    $($variant::KIND => Self::$variant($variant::cast(node)?),)+
+                    _ => return None,
+                };
+                Some(node)
+            }
+        }
+    };
 }
 
-ast_node!(Root, NODE_ROOT);
+///////////////
+// Root node //
+///////////////
+
+ast_node! {
+    #[from(NODE_ROOT)]
+    /// Root node
+    struct Root;
+}
+
+/////////////////
+// Basic nodes //
+/////////////////
+
+ast_node! {
+    #[from(NODE_NAME)]
+    /// Represents a name.
+    ///
+    /// Can be name of an object/record field, endpoint name, ...
+    struct Name;
+}
+
+//////////////////////////////////
+// Direct children of root node //
+//////////////////////////////////
+
+ast_node! {
+    #[from(NODE_API_NAME)]
+    /// Name of the API in the spec.
+    struct ApiName;
+}
+
+impl ApiName {
+    pub fn value(&self) -> Option<Value> {
+        self.0.children().find_map(Value::cast)
+    }
+}
+
+////////////
+// Values //
+////////////
+
+ast_node! {
+    #[from(TOKEN_STRING)]
+    /// String value.
+    struct String;
+}
+
+ast_node! {
+    #[from(TOKEN_INTEGER)]
+    /// Integer value.
+    struct Integer;
+}
+
+ast_node! {
+    #[from(TOKEN_FLOAT)]
+    /// Float value.
+    struct Float;
+}
+
+ast_node! {
+    #[from(NODE_BOOL_VALUE)]
+    /// Boolean value.
+    struct Bool;
+}
+
+ast_node! {
+    #[from(NODE_OBJECT)]
+    /// An object.
+    struct Object;
+}
+
+ast_node! {
+    #[from(NODE_OBJECT_FIELD)]
+    /// Field of an object.
+    struct ObjectField;
+}
+
+ast_node! {
+    #[from(
+        String,
+        Integer,
+        Float,
+        Bool,
+        Object,
+    )]
+    /// A value of any type.
+    enum Value;
+}
